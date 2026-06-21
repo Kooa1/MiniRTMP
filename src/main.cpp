646ee0c8bad60e7bb3247f6e6006e288c4cba5d8
862190424@qq.com <862190424@qq.com>
2026-06-21 15:04:53 +0800

    Access monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+#include "server/server.h"
+
 int main() {
     constexpr int RTMP_PORT = 1934;
 
     std::cout << "MiniRTMP starting on port: " << RTMP_PORT << '\n';
+    server::Server server(RTMP_PORT);
+    server.start();
 
     return 0;
 }
