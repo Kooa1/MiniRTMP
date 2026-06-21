17bc04f3a9faa6a568788d6802fd4b28a3b16f1a
862190424@qq.com <862190424@qq.com>
2026-06-21 18:05:05 +0800

    remove namespace

--- src/protocol/RtmpMessage.h
+++ src/protocol/RtmpMessage.h
@@ -8,13 +8,11 @@
 #include <cstdint>
 #include <vector>
 
-namespace protocol {
-    struct RtmpMessage {
-        uint32_t timestamp_ = 0; // 绝对时间戳（ms）
-        uint32_t stream_id_ = 0; // 消息流 ID（小端序原始值）
-        uint8_t type_ = 0; // 消息类型（20=Command, 1=SetChunkSize, ...）
-        std::vector<uint8_t> payload; // 完整载荷
-    };
-}
+struct RtmpMessage {
+    uint32_t timestamp_ = 0; // 绝对时间戳（ms）
+    uint32_t stream_id_ = 0; // 消息流 ID（小端序原始值）
+    uint8_t type_ = 0; // 消息类型（20=Command, 1=SetChunkSize, ...）
+    std::vector<uint8_t> payload; // 完整载荷
+};
 
 #endif //MINIRTMP_RTMPMESSAGE_H
